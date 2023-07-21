3aba2241d026e88a11ef6ba2d18b6da2c72f2ec6
Joseph Argento <josepha@datalogics.com>
2023-07-21 08:03:32 -0500

    Add OutputPreview sample.
    To demonstrate generating an OutputPreview image from a PDF page, this is typically used during Soft Proofing prior to final print.

---
+++ Images/OutputPreview/OutputPreview.cs
@@ -0,0 +1,82 @@
+using Datalogics.PDFL;
+using System;
+using System.Collections.Generic;
+
+/*
+ * This sample demonstrates creating an Output Preview Image which is used during Soft Proofing prior to printing to visualize combining different Colorants.
+ *
+ * 
+ * Copyright (c)2023, Datalogics, Inc. All rights reserved.
+ *
+ */
+
+namespace OutputPreview
+{
+    class OutputPreview
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OutputPreview Sample:");
+
+            String sInput = Library.ResourceDirectory + "Sample_Input/spotcolors1.pdf";
+
+            // Here you specify the Colorant names of interest
+            List<String> colorantsToUse = new List<string>() { "Yellow", "Black" };
+
+            List<String> colorantsToUse2 = new List<string>() { "PANTONE 554 CVC", "PANTONE 814 2X CVC", "PANTONE 185 2X CVC" };
+
+            using (Library lib = new Library())
+            {
+                using (Document doc = new Document(sInput))
+                {
+                    using (Page pg = doc.GetPage(0))
+                    {
+                        // Get all inks that are present on the page
+                        List<Ink> inks = (List<Ink>)pg.ListInks();
+
+                        List<SeparationColorSpace> colorants = new List<SeparationColorSpace>();
+
+                        foreach (Ink theInk in inks)
+                        {
+                            foreach (String theColorant in colorantsToUse)
+                            {
+                                if (theInk.ColorantName == theColorant)
+                                {
+                                    colorants.Add(new SeparationColorSpace(pg, theInk));
+                                }
+                            }
+                        }
+
+                        List<SeparationColorSpace> colorants2 = new List<SeparationColorSpace>();
+
+                        foreach (Ink theInk in inks)
+                        {
+                            foreach (String theColorant in colorantsToUse2)
+                            {
+                                if (theInk.ColorantName == theColorant)
+                                {
+                                    colorants2.Add(new SeparationColorSpace(pg, theInk));
+                                }
+                            }
+                        }
+
+                        PageImageParams pip = new PageImageParams();
+                        pip.HorizontalResolution = 300;
+                        pip.VerticalResolution = 300;
+
+                        ImageSaveParams sp = new ImageSaveParams();
+
+                        // Create Output Preview images using the Specified Colorants
+                        Datalogics.PDFL.Image image = pg.GetOutputPreviewImage(pg.CropBox, pip, colorants);
+
+                        image.Save(String.Format("OutputPreview_{0}_{1}.tiff", colorantsToUse[0], colorantsToUse[1]), ImageType.TIFF);
+
+                        Datalogics.PDFL.Image image2 = pg.GetOutputPreviewImage(pg.CropBox, pip, colorants2);
+
+                        image2.Save(String.Format("OutputPreview_{0}_{1}.tiff", colorantsToUse2[0], colorantsToUse2[1]), ImageType.TIFF);
+                    }
+                }
+            }
+        }
+    }
+}
