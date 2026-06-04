654b09c45e40cd5d5c56360c1a0f9fe0324302a4
Robert Boehne <robb@datalogics.com>
2026-06-03 22:11:31 -0700

    Correct (or remove) links.

--- DocumentConversion/Factur-XConverter/Factur-XConverter.cs
+++ DocumentConversion/Factur-XConverter/Factur-XConverter.cs
@@ -5,9 +5,6 @@ using Datalogics.PDFL;
  * 
  * This sample demonstrates converting the input PDF with the input Invoice XML to a Factur-X compliant PDF.
  *
- * For more detail see the description of the Factur-XConverter sample program on our Developer’s site, 
- * http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/net-core-sample-programs/converting-and-merging-pdf-content/#facturxconverter
- * 
  * Copyright (c) 2022-2023, Datalogics, Inc. All rights reserved.
  *
  */
